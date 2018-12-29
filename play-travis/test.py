539a61b4e4c29d7c09002225029f83eae59d6653
Gryaznov Artem <gryaznovart186@gmail.com>
2018-12-29 13:24:19 +0500

    For push file test.py

--- play-travis/test.py
+++ play-travis/test.py
@@ -1,7 +1,8 @@
 import unittest
-class NumbersTest(unittest.TestCase):
 
+class NumbersTest(unittest.TestCase):
     def test_equal(self):
-        self.assertEqual(1 + 1, 1)
+        self.assertEqual(1 + 1, 2)
+
 if __name__ == '__main__':
     unittest.main()
